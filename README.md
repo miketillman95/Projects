357d36add562cb8a8874e163fa77eee916bf04be
mike <matillman95@gmail.com>
2023-01-26 21:09:35 -0700

    recommit

--- README.md
+++ README.md
@@ -2,14 +2,10 @@ start venv = python3 -m venv venv && source venv/bin/activate
 
 to run server: python manage.py runserver
 
-
+If pip install does not work in venv run: python3 -m pip install <dependency>
 
 add images
-finish project detail page
-add links column to db
-add mobile first tag
-change background color of page
-add nav bar
+Fix the column to display info
 push to github
 deploy
 
--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ybhkh!@xmd3%ncru7q@o4&!_#w$_6#djplj&+r3)bkc#86cuo4'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -117,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static',]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
